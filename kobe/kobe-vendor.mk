6c193634faaae6f419dc706f89b124ff9a6f3d17
Quarx2k <agent00791@gmail.com>
2012-07-26 16:34:49 +0700

    cleanup kobe vendor

--- kobe/kobe-vendor.mk
+++ kobe/kobe-vendor.mk
@@ -12,125 +12,139 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+common_vendor_path = vendor/motorola/jordan-common
+vendor_path = vendor/motorola/kobe
 
+# Kobe stuff
+PRODUCT_COPY_FILES += \
+	$(vendor_path)/lib/libmotdb.so:system/lib/libmotdb.so \
+	$(vendor_path)/lib/libmsl_interface.so:system/lib/libmsl_interface.so \
+	$(vendor_path)/lib/libril-moto-umts-1.so:system/lib/libril-moto-umts-1.so \
+        $(vendor_path)/etc/mot_ise_imager_cfg.bin:system/etc/mot_ise_imager_cfg.bin \
+	$(vendor_path)/lib/libkobecamera.so:system/lib/libkobecamera.so \
+	$(vendor_path)/bin/protocol_driver:system/bin/protocol_driver \
+	$(vendor_path)/bin/opprofdaemon:system/bin/opprofdaemon \
+	$(vendor_path)/bin/nvm_daemon:system/bin/nvm_daemon \
+
+PRODUCT_COPY_FILES += \
+        $(vendor_path)/etc/motorola/ap_flex_version.txt:system/etc/motorola/ap_flex_version.txt \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio:system/etc/motorola/bp_nvm_default/File_Audio \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio1_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio1_AMR_WB \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio2:system/etc/motorola/bp_nvm_default/File_Audio2 \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio2_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio2_AMR_WB \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio3:system/etc/motorola/bp_nvm_default/File_Audio3 \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio3_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio3_AMR_WB \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio4:system/etc/motorola/bp_nvm_default/File_Audio4 \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio4_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio4_AMR_WB \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio5:system/etc/motorola/bp_nvm_default/File_Audio5 \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio5_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio5_AMR_WB \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio6:system/etc/motorola/bp_nvm_default/File_Audio6 \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio7:system/etc/motorola/bp_nvm_default/File_Audio7 \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Audio8:system/etc/motorola/bp_nvm_default/File_Audio8 \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_GPRS:system/etc/motorola/bp_nvm_default/File_GPRS \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_GSM:system/etc/motorola/bp_nvm_default/File_GSM \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Logger:system/etc/motorola/bp_nvm_default/File_Logger \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_Seem_Flex_Tables:system/etc/motorola/bp_nvm_default/File_Seem_Flex_Tables \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_UMA:system/etc/motorola/bp_nvm_default/File_UMA \
+        $(vendor_path)/etc/motorola/bp_nvm_default/generic_pds_init:system/etc/motorola/bp_nvm_default/generic_pds_init \
+        $(vendor_path)/etc/motorola/bp_nvm_default/File_UMA:system/etc/motorola/bp_nvm_default/File_UMA \
+        $(vendor_path)/etc/motorola/comm_drv/commdrv_fs.sh:system/etc/motorola/comm_drv/commdrv_fs.sh \
+        $(vendor_path)/etc/motorola/comm_drv/mmins_cust_emergency_number.cfg:system/etc/motorola/comm_drv/mmins_cust_emergency_number.cfg \
+        $(vendor_path)/etc/motorola/comm_drv/mmins_settings.cfg:system/etc/motorola/comm_drv/mmins_settings.cfg \
+        $(vendor_path)/etc/motorola/comm_drv/mmins_telephony.cfg:system/etc/motorola/comm_drv/mmins_telephony.cfg \
+        $(vendor_path)/etc/motorola/comm_drv/mmins_user_settings.cfg:system/etc/motorola/comm_drv/mmins_user_settings.cfg \
+        $(vendor_path)/etc/motorola/comm_drv/plmn_text_table.bin:system/etc/motorola/comm_drv/plmn_text_table.bin \
+        $(vendor_path)/etc/motorola/comm_drv/PLMN_VERSION.txt:system/etc/motorola/comm_drv/PLMN_VERSION.txt
+
+
+# Moto-common stuff
 # LIBS
 PRODUCT_COPY_FILES += \
-	vendor/motorola/kobe/lib/libaudio.so:obj/lib/libaudio.so \
-	vendor/motorola/kobe/lib/libaudio.so:system/lib/libaudio.so \
-	vendor/motorola/kobe/lib/libIMGegl.so:system/lib/libIMGegl.so \
-	vendor/motorola/kobe/lib/libpvr2d.so:system/lib/libpvr2d.so \
-	vendor/motorola/kobe/lib/libpvrANDROID_WSEGL.so:system/lib/libpvrANDROID_WSEGL.so \
-	vendor/motorola/kobe/lib/libPVRScopeServices.so:system/lib/libPVRScopeServices.so \
-	vendor/motorola/kobe/lib/libsrv_init.so:system/lib/libsrv_init.so \
-	vendor/motorola/kobe/lib/libsrv_um.so:system/lib/libsrv_um.so \
-	vendor/motorola/kobe/lib/libbattd.so:system/lib/libbattd.so \
-	vendor/motorola/kobe/lib/libgki.so:system/lib/libgki.so \
-	vendor/motorola/kobe/lib/libglslcompiler.so:system/lib/libglslcompiler.so \
-	vendor/motorola/kobe/lib/libmotdb.so:system/lib/libmotdb.so \
-	vendor/motorola/kobe/lib/libmsl_interface.so:system/lib/libmsl_interface.so \
-	vendor/motorola/kobe/lib/libssmgr.so:system/lib/libssmgr.so \
-	vendor/motorola/kobe/lib/libusc.so:system/lib/libusc.so \
-	vendor/motorola/kobe/lib/libril-moto-umts-1.so:system/lib/libril-moto-umts-1.so \
-	vendor/motorola/kobe/lib/hw/gralloc.omap3.so:system/lib/hw/gralloc.omap3.so \
-	vendor/motorola/kobe/lib/hw/gps.kobe.so:system/lib/hw/gps.kobe.so \
-	vendor/motorola/kobe/lib/dsp/baseimage.dof:system/lib/dsp/baseimage.dof \
-	vendor/motorola/kobe/lib/dsp/conversions.dll64P:system/lib/dsp/conversions.dll64P \
-	vendor/motorola/kobe/lib/dsp/h264vdec_sn.dll64P:system/lib/dsp/h264vdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/h264venc_sn.dll64P:system/lib/dsp/h264venc_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/jpegdec_sn.dll64P:system/lib/dsp/jpegdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/jpegenc_sn.dll64P:system/lib/dsp/jpegenc_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/m4venc_sn.dll64P:system/lib/dsp/m4venc_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/mp3dec_sn.dll64P:system/lib/dsp/mp3dec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/mp4vdec_sn.dll64P:system/lib/dsp/mp4vdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/mpeg4aacdec_sn.dll64P:system/lib/dsp/mpeg4aacdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/mpeg4aacenc_sn.dll64P:system/lib/dsp/mpeg4aacenc_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/nbamrdec_sn.dll64P:system/lib/dsp/nbamrdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/nbamrenc_sn.dll64P:system/lib/dsp/nbamrenc_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/postprocessor_dualout.dll64P:system/lib/dsp/postprocessor_dualout.dll64P \
-	vendor/motorola/kobe/lib/dsp/ringio.dll64P:system/lib/dsp/ringio.dll64P \
-	vendor/motorola/kobe/lib/dsp/usn.dll64P:system/lib/dsp/usn.dll64P \
-	vendor/motorola/kobe/lib/dsp/wbamrdec_sn.dll64P:system/lib/dsp/wbamrdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/wbamrenc_sn.dll64P:system/lib/dsp/wbamrenc_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/wmadec_sn.dll64P:system/lib/dsp/wmadec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/720p_h264vdec_sn.dll64P:system/lib/dsp/720p_h264vdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/mpeg4ingenc_sn.dll64P:system/lib/dsp/mpeg4ingenc_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/h264ingdec_sn.dll64P:system/lib/dsp/h264ingdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/mpeg4ingdec_sn.dll64P:system/lib/dsp/mpeg4ingdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/720p_mp4vdec_sn.dll64P:system/lib/dsp/720p_mp4vdec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/wmadec_sn.dll64P:system/lib/dsp/wmadec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/wmv9dec_sn.dll64P:system/lib/dsp/wmv9dec_sn.dll64P \
-	vendor/motorola/kobe/lib/dsp/vpp_sn.dll64P:system/lib/dsp/vpp_sn.dll64P \
-	vendor/motorola/kobe/lib/egl/libEGL_POWERVR_SGX530_125.so:system/lib/egl/libEGL_POWERVR_SGX530_125.so \
-	vendor/motorola/kobe/lib/egl/libGLESv1_CM_POWERVR_SGX530_125.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX530_125.so \
-	vendor/motorola/kobe/lib/egl/libGLESv2_POWERVR_SGX530_125.so:system/lib/egl/libGLESv2_POWERVR_SGX530_125.so \
-	vendor/motorola/kobe/lib/libOMX.TI.720P.Decoder.so:system/lib/libOMX.TI.720P.Decoder.so \
-	vendor/motorola/kobe/lib/libFMRadio.so:system/lib/libFMRadio.so \
-	vendor/motorola/kobe/lib/libfmradio_jni.so:system/lib/libfmradio_jni.so \
-	vendor/motorola/kobe/lib/libfmradioplayer.so:system/lib/libfmradioplayer.so \
-	vendor/motorola/kobe/lib/libkobecamera.so:system/lib/libkobecamera.so \
-#Moto etc
+	$(common_vendor_path)/lib/libaudio.so:obj/lib/libaudio.so \
+	$(common_vendor_path)/lib/libaudio.so:system/lib/libaudio.so \
+	$(common_vendor_path)/lib/libbattd.so:system/lib/libbattd.so \
+	$(common_vendor_path)/lib/libgki.so:system/lib/libgki.so \
+	$(common_vendor_path)/lib/libssmgr.so:system/lib/libssmgr.so \
+	$(common_vendor_path)/lib/hw/gps.jordan.so:system/lib/hw/gps.kobe.so \
+	$(common_vendor_path)/lib/dsp/baseimage.dof:system/lib/dsp/baseimage.dof \
+	$(common_vendor_path)/lib/dsp/conversions.dll64P:system/lib/dsp/conversions.dll64P \
+	$(common_vendor_path)/lib/dsp/h264vdec_sn.dll64P:system/lib/dsp/h264vdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/h264venc_sn.dll64P:system/lib/dsp/h264venc_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/jpegdec_sn.dll64P:system/lib/dsp/jpegdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/jpegenc_sn.dll64P:system/lib/dsp/jpegenc_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/m4venc_sn.dll64P:system/lib/dsp/m4venc_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/mp3dec_sn.dll64P:system/lib/dsp/mp3dec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/mp4vdec_sn.dll64P:system/lib/dsp/mp4vdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/mpeg4aacdec_sn.dll64P:system/lib/dsp/mpeg4aacdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/mpeg4aacenc_sn.dll64P:system/lib/dsp/mpeg4aacenc_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/nbamrdec_sn.dll64P:system/lib/dsp/nbamrdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/nbamrenc_sn.dll64P:system/lib/dsp/nbamrenc_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/postprocessor_dualout.dll64P:system/lib/dsp/postprocessor_dualout.dll64P \
+	$(common_vendor_path)/lib/dsp/ringio.dll64P:system/lib/dsp/ringio.dll64P \
+	$(common_vendor_path)/lib/dsp/usn.dll64P:system/lib/dsp/usn.dll64P \
+	$(common_vendor_path)/lib/dsp/wbamrdec_sn.dll64P:system/lib/dsp/wbamrdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/wbamrenc_sn.dll64P:system/lib/dsp/wbamrenc_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/wmadec_sn.dll64P:system/lib/dsp/wmadec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/720p_h264vdec_sn.dll64P:system/lib/dsp/720p_h264vdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/mpeg4ingenc_sn.dll64P:system/lib/dsp/mpeg4ingenc_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/h264ingdec_sn.dll64P:system/lib/dsp/h264ingdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/mpeg4ingdec_sn.dll64P:system/lib/dsp/mpeg4ingdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/720p_mp4vdec_sn.dll64P:system/lib/dsp/720p_mp4vdec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/wmadec_sn.dll64P:system/lib/dsp/wmadec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/wmv9dec_sn.dll64P:system/lib/dsp/wmv9dec_sn.dll64P \
+	$(common_vendor_path)/lib/dsp/vpp_sn.dll64P:system/lib/dsp/vpp_sn.dll64P \
+	$(common_vendor_path)/lib/libOMX.TI.720P.Decoder.so:system/lib/libOMX.TI.720P.Decoder.so \
+	$(common_vendor_path)/lib/libFMRadio.so:system/lib/libFMRadio.so \
+	$(common_vendor_path)/lib/libfmradio_jni.so:system/lib/libfmradio_jni.so \
+	$(common_vendor_path)/lib/libfmradioplayer.so:system/lib/libfmradioplayer.so \
+
+#etc
 PRODUCT_COPY_FILES += \
-        vendor/motorola/kobe/etc/firmware/wl1271.bin:system/etc/firmware/wl1271.bin \
-        vendor/motorola/kobe/etc/wifi/fw_tiwlan_ap.bin:system/etc/wifi/fw_tiwlan_ap.bin \
-        vendor/motorola/kobe/etc/wifi/fw_wlan1271.bin:system/etc/wifi/fw_wlan1271.bin \
-        vendor/motorola/kobe/etc/mot_ise_imager_cfg.bin:system/etc/mot_ise_imager_cfg.bin \
-	vendor/motorola/kobe/etc/motorola/ap_flex_version.txt:system/etc/motorola/ap_flex_version.txt \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio:system/etc/motorola/bp_nvm_default/File_Audio \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio1_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio1_AMR_WB \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio2:system/etc/motorola/bp_nvm_default/File_Audio2 \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio2_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio2_AMR_WB \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio3:system/etc/motorola/bp_nvm_default/File_Audio3 \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio3_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio3_AMR_WB \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio4:system/etc/motorola/bp_nvm_default/File_Audio4 \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio4_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio4_AMR_WB \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio5:system/etc/motorola/bp_nvm_default/File_Audio5 \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio5_AMR_WB:system/etc/motorola/bp_nvm_default/File_Audio5_AMR_WB \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio6:system/etc/motorola/bp_nvm_default/File_Audio6 \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio7:system/etc/motorola/bp_nvm_default/File_Audio7 \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Audio8:system/etc/motorola/bp_nvm_default/File_Audio8 \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_GPRS:system/etc/motorola/bp_nvm_default/File_GPRS \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_GSM:system/etc/motorola/bp_nvm_default/File_GSM \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Logger:system/etc/motorola/bp_nvm_default/File_Logger \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_Seem_Flex_Tables:system/etc/motorola/bp_nvm_default/File_Seem_Flex_Tables \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_UMA:system/etc/motorola/bp_nvm_default/File_UMA \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/generic_pds_init:system/etc/motorola/bp_nvm_default/generic_pds_init \
-	vendor/motorola/kobe/etc/motorola/bp_nvm_default/File_UMA:system/etc/motorola/bp_nvm_default/File_UMA \
-	vendor/motorola/kobe/etc/motorola/comm_drv/commdrv_fs.sh:system/etc/motorola/comm_drv/commdrv_fs.sh \
-	vendor/motorola/kobe/etc/motorola/comm_drv/mmins_cust_emergency_number.cfg:system/etc/motorola/comm_drv/mmins_cust_emergency_number.cfg \
-	vendor/motorola/kobe/etc/motorola/comm_drv/mmins_settings.cfg:system/etc/motorola/comm_drv/mmins_settings.cfg \
-	vendor/motorola/kobe/etc/motorola/comm_drv/mmins_telephony.cfg:system/etc/motorola/comm_drv/mmins_telephony.cfg \
-	vendor/motorola/kobe/etc/motorola/comm_drv/mmins_user_settings.cfg:system/etc/motorola/comm_drv/mmins_user_settings.cfg \
-	vendor/motorola/kobe/etc/motorola/comm_drv/plmn_text_table.bin:system/etc/motorola/comm_drv/plmn_text_table.bin \
-	vendor/motorola/kobe/etc/motorola/comm_drv/PLMN_VERSION.txt:system/etc/motorola/comm_drv/PLMN_VERSION.txt
+        $(common_vendor_path)/etc/firmware/wl1271.bin:system/etc/firmware/wl1271.bin \
+        $(common_vendor_path)/etc/wifi/fw_tiwlan_ap.bin:system/etc/wifi/fw_tiwlan_ap.bin \
+        $(common_vendor_path)/etc/wifi/fw_wlan1271.bin:system/etc/wifi/fw_wlan1271.bin \
 
 #bin
-
 PRODUCT_COPY_FILES += \
-	vendor/motorola/kobe/bin/pvrsrvinit:system/bin/pvrsrvinit \
-        vendor/motorola/kobe/bin/battd:system/bin/battd \
-	vendor/motorola/kobe/bin/protocol_driver:system/bin/protocol_driver \
-	vendor/motorola/kobe/bin/opprofdaemon:system/bin/opprofdaemon \
-	vendor/motorola/kobe/bin/nvm_daemon:system/bin/nvm_daemon \
-	vendor/motorola/kobe/bin/gkisystem:system/bin/gkisystem \
-	vendor/motorola/kobe/bin/ap_gain.bin:system/bin/ap_gain.bin \
-	vendor/motorola/kobe/bin/fmradioserver:system/bin/fmradioserver \
-	vendor/motorola/kobe/bin/brcm_guci_drv:system/bin/brcm_guci_drv \
-	vendor/motorola/kobe/bin/akmd2:system/bin/akmd2 \
-	vendor/motorola/kobe/bin/usbd:system/bin/usbd \
-	vendor/motorola/kobe/bin/location:system/bin/location \
-	vendor/motorola/kobe/bin/ssmgrd:system/bin/ssmgrd
+        $(common_vendor_path)/bin/battd:system/bin/battd \
+	$(common_vendor_path)/bin/gkisystem:system/bin/gkisystem \
+	$(common_vendor_path)/bin/ap_gain.bin:system/bin/ap_gain.bin \
+	$(common_vendor_path)/bin/fmradioserver:system/bin/fmradioserver \
+	$(common_vendor_path)/bin/brcm_guci_drv:system/bin/brcm_guci_drv \
+	$(common_vendor_path)/bin/akmd2:system/bin/akmd2 \
+	$(common_vendor_path)/bin/usbd:system/bin/usbd \
+	$(common_vendor_path)/bin/location:system/bin/location \
+	$(common_vendor_path)/bin/ssmgrd:system/bin/ssmgrd
+
+# TI SGX DDK 1.8 ES 5.x
+PRODUCT_COPY_FILES += \
+	$(common_vendor_path)/ti_sgx_es5.x/gralloc.omap3.so:/system/lib/hw/gralloc.omap3.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libEGL_POWERVR_SGX530_125.so:/system/lib/egl/libEGL_POWERVR_SGX530_125.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libGLESv1_CM_POWERVR_SGX530_125.so:/system/lib/egl/libGLESv1_CM_POWERVR_SGX530_125.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libGLESv2_POWERVR_SGX530_125.so:/system/lib/egl/libGLESv2_POWERVR_SGX530_125.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libpvr2d.so:/system/lib/libpvr2d.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libeglinfo.so:/system/lib/libeglinfo.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libusc.so:/system/lib/libusc.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libglslcompiler.so:/system/lib/libglslcompiler.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libpvrANDROID_WSEGL.so:/system/lib/libpvrANDROID_WSEGL.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libsrv_um.so:/system/lib/libsrv_um.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libIMGegl.so:/system/lib/libIMGegl.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libsrv_init.so:/system/lib/libsrv_init.so \
+	$(common_vendor_path)/ti_sgx_es5.x/libPVRScopeServices.so:/system/lib/libPVRScopeServices.so \
+	$(common_vendor_path)/ti_sgx_es5.x/pvrsrvctl:/system/bin/pvrsrvctl \
+	$(common_vendor_path)/ti_sgx_es5.x/pvrsrvinit:/system/bin/pvrsrvinit \
 
 #location proxy
 PRODUCT_COPY_FILES += \
-	vendor/motorola/kobe/framework/com.motorola.android.location.jar:system/framework/com.motorola.android.location.jar
+	$(common_vendor_path)/framework/com.motorola.android.location.jar:system/framework/com.motorola.android.location.jar
 
 #app
 PRODUCT_COPY_FILES += \
-	vendor/motorola/kobe/app/FMRadioService.apk:system/app/FMRadioService.apk \
-	vendor/motorola/kobe/app/MotoPhonePortal.apk:system/app/MotoPhonePortal.apk \
-	vendor/motorola/kobe/app/AudioEffectSettings.apk:system/app/AudioEffectSettings.apk \
-	vendor/motorola/kobe/app/CompassCalibrate.apk:system/app/CompassCalibrate.apk 
+	$(common_vendor_path)/app/FMRadioService.apk:system/app/FMRadioService.apk \
+	$(common_vendor_path)/app/MotoPhonePortal.apk:system/app/MotoPhonePortal.apk \
+	$(common_vendor_path)/app/AudioEffectSettings.apk:system/app/AudioEffectSettings.apk \
+	$(common_vendor_path)/app/CompassCalibrate.apk:system/app/CompassCalibrate.apk 
 
 #extra
 PRODUCT_COPY_FILES += \
-	vendor/motorola/kobe/media/Moto.ogg:system/media/audio/ringtones/Moto.ogg
+	$(common_vendor_path)/media/Moto.ogg:system/media/audio/ringtones/Moto.ogg
