9acc5a0f04ec564c8eb3b2a5488cdda3e914f9d2
Quarx2k <agent00791@gmail.com>
2012-09-12 20:36:03 +0700

    revert usage libfnc for ril

--- jordan-common/jordan-vendor.mk
+++ jordan-common/jordan-vendor.mk
@@ -36,6 +36,13 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/lib/libsoccamera.so:system/lib/libsoccamera.so \
 	$(vendor_path)/lib/libbayercamera.so:system/lib/libbayercamera.so \
 	$(vendor_path)/lib/libHPImgApi.so:system/lib/libHPImgApi.so \
+	$(vendor_path)/lib/libbattd.so:system/lib/libbattd.so \
+	$(vendor_path)/lib/libcryptoki.so:system/lib/libcryptoki.so \
+	$(vendor_path)/lib/libmotodbgutils.so:system/lib/libmotodbgutils.so \
+	$(vendor_path)/lib/libsmapi.so:system/lib/libsmapi.so \
+	$(vendor_path)/lib/libtpa.so:system/lib/libtpa.so \
+	$(vendor_path)/lib/libtpa_core.so:system/lib/libtpa_core.so \
+	$(vendor_path)/lib/libpanic_daemon.so:system/lib/libpanic_daemon.so \
 	$(vendor_path)/lib/hw/gps.jordan.so:system/lib/hw/gps.jordan.so \
 	$(vendor_path)/lib/dsp/baseimage.dof:system/lib/dsp/baseimage.dof \
 	$(vendor_path)/lib/dsp/conversions.dll64P:system/lib/dsp/conversions.dll64P \
@@ -174,6 +181,8 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/bin/usbd:system/bin/usbd \
 	$(vendor_path)/bin/location:system/bin/location \
 	$(vendor_path)/bin/ssmgrd:system/bin/ssmgrd \
+	$(vendor_path)/bin/secclkd:system/bin/secclkd \
+	$(vendor_path)/bin/panic_daemon:system/bin/panic_daemon \
 
 # Specific binaries
 ifeq ($(TARGET_PRODUCT),cm_mb526)
