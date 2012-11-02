514b8768f6a1f7a8fdb458049c70651e304d9cef
Quarx2k <agent00791@gmail.com>
2012-11-02 18:52:06 +0700

    Revert "Revert "revert usage libfnc for ril""
    This reverts commit 17a757ff260ea704029f2b67c570a0a5860a107f.
    
    Conflicts:
    
    	jordan-common/jordan-vendor.mk

--- jordan-common/jordan-vendor.mk
+++ jordan-common/jordan-vendor.mk
@@ -36,6 +36,13 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/lib/libsoccamera.so:system/lib/libsoccamera.so \
 	$(vendor_path)/lib/libbayercamera.so:system/lib/libbayercamera.so \
 	$(vendor_path)/lib/libHPImgApi.so:system/lib/libHPImgApi.so \
+	$(vendor_path)/lib/libbattd.so:system/lib/libbattd.so \
+	$(vendor_path)/lib/libcryptoki.so:system/lib/libcryptoki.so \
+	$(vendor_path)/lib/libmotodbgutils.so:system/lib/libmotodbgutils.so \
+	$(vendor_path)/lib/libsmapi.so:system/lib/libsmapi.so \
+	$(vendor_path)/lib/libtpa.so:system/lib/libtpa.so \
+	$(vendor_path)/lib/libtpa_core.so:system/lib/libtpa_core.so \
+	$(vendor_path)/lib/libpanic_daemon.so:system/lib/libpanic_daemon.so \
 	$(vendor_path)/lib/libPERF.so:system/lib/libPERF.so \
 	$(vendor_path)/lib/hw/gps.jordan.so:system/lib/hw/gps.jordan.so \
 	$(vendor_path)/lib/dsp/baseimage.dof:system/lib/dsp/baseimage.dof \
@@ -180,6 +187,9 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/bin/ntfs-3g:system/bin/ntfs-3g \
 	$(vendor_path)/bin/ntfs-3g.probe:system/bin/ntfs-3g.probe \
 	$(vendor_path)/bin/fsck.exfat:system/bin/fsck.exfat \
+	$(vendor_path)/bin/secclkd:system/bin/secclkd \
+	$(vendor_path)/bin/panic_daemon:system/bin/panic_daemon \
+
 
 # Specific binaries
 ifeq ($(TARGET_PRODUCT),cm_mb526)
