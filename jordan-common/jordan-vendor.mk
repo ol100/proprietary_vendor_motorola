b405e801ce5e254de92cb8e379ddbe8b99f518d9
Quarx2k <agent00791@gmail.com>
2013-05-11 18:36:17 +0700

    remove usbd

--- jordan-common/jordan-vendor.mk
+++ jordan-common/jordan-vendor.mk
@@ -179,7 +179,6 @@ PRODUCT_COPY_FILES += \
 	$(vendor_path)/bin/fmradioserver:system/bin/fmradioserver \
 	$(vendor_path)/bin/brcm_guci_drv:system/bin/brcm_guci_drv \
 	$(vendor_path)/bin/akmd2:system/bin/akmd2 \
-	$(vendor_path)/bin/usbd:system/bin/usbd \
 	$(vendor_path)/bin/location:system/bin/location \
 	$(vendor_path)/bin/ssmgrd:system/bin/ssmgrd \
 	$(vendor_path)/bin/mkfs.exfat:system/bin/mkfs.exfat \
